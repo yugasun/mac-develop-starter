4068dae19cf98bf11bbacb4c419eab86aa36b620
yugasun <yuga_sun@163.com>
2022-07-18 10:00:26 +0800

    fix: add docker cask

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ WORK_DIR="$(pwd)"
 HOST_NAME=mbp
 COMMAND_LIST="$WORK_DIR/command/list.txt"
 APP_LIST="$(pwd)/app/list.txt"
+NODE_VERSION=v16.16.0
 
 # set hostname
 sudo scutil --set HostName $HOST_NAME
@@ -17,7 +18,7 @@ sudo scutil --set HostName $HOST_NAME
 cat $COMMAND_LIST | xargs brew install
 
 # install apps by homebrew
-cat $APP_LIST | xargs brew install
+cat $APP_LIST | xargs brew install --cask
 
 # Initialize git environment
 /bin/sh -c $WORK_DIR/git.init.sh
@@ -26,3 +27,16 @@ cat $APP_LIST | xargs brew install
 ZSH_PROXY_PLUGIN_DIR=~/.oh-my-zsh/custom/plugins/zsh-proxy
 rm -rf $ZSH_PROXY_PLUGIN_DIR
 git clone https://github.com/yugasun/zsh-proxy.git $ZSH_PROXY_PLUGIN_DIR
+
+# install lts node
+fnm install $NODE_VERSION
+fnm alias $NODE_VERSION default
+fnm use default
+node -v
+
+# init .zshrc
+copy $WORK_DIR/.zshrc ~/.zshrc
+source ~/.zshrc
+
+# install nrm
+pnpm install nrm -g
